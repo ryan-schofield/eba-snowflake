a08cf2294f9fb82f0c08d8fd75e3f2988c8c0cbf
Ryan Schofield <rschofield@eidebailly.com>
2019-10-15 17:14:43 -0600

    Add viz folder

---
+++ dbt/models/dw/flyby_readings.sql
@@ -0,0 +1,24 @@
+SELECT fb.flyby_id
+	,fb.flyby_name
+	,cd.name AS chem_name
+	,cd.formula AS chem_formula
+	,inms.source
+	,CASE 
+		WHEN cd.formula IN ('H2', 'CH4', 'CO2', 'H2O')
+			THEN TRUE
+		ELSE FALSE
+		END AS is_thumbprint_of_life
+	,sum(inms.high_counts) AS high_counts
+	,sum(inms.low_counts) AS low_counts
+FROM {{ref('flybys')}} fb
+INNER JOIN {{ref('inms_readings')}} inms
+	ON inms.time_stamp >= fb.window_start
+		AND inms.time_stamp <= fb.window_end
+INNER JOIN {{ref('chem_data')}} cd
+	ON cd.peak = inms.mass_per_charge
+WHERE fb.targeted = TRUE
+GROUP BY fb.flyby_id
+	,fb.flyby_name
+	,cd.name
+	,cd.formula
+	,inms.source
